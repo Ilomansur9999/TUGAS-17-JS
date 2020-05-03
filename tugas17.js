e269b365815084f41ed6e859675b36075728973a
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 21:30:47 +0700

    TUGAS 17 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas17.js
@@ -0,0 +1,22 @@
+function Ubah() {
+	var kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+	console.log(kalimat1.toLowerCase())
+	console.log(kalimat1.toUpperCase())
+
+
+
+	var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+	console.log(kalimat2.toLowerCase())
+	console.log(kalimat2.toUpperCase())
+}
+Ubah();
+
+
+
+
+// C:\Users\USER\Documents\JS\javascript>node tugas17.js
+// saya belajar bahasa pemrograman dengan khusuk
+// SAYA BELAJAR BAHASA PEMROGRAMAN DENGAN KHUSUK
+// allen sedang memancing ikan di sungai, dia mendapat ikan gabus 3 ekor dan langsung dijual
+// ALLEN SEDANG MEMANCING IKAN DI SUNGAI, DIA MENDAPAT IKAN GABUS 3 EKOR DAN LANGSUNG DIJUAL
+
